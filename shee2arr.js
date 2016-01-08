c9fbbc20453cc0351bfce1eab6b10b804d80866c
Mithgol the Webmaster <getgit@mithgol.ru>
2016-01-08 23:39:55 +0300

    add a (presumably) working version of this module's source code

--- shee2arr.js
+++ shee2arr.js
@@ -1,3 +1,19 @@
-module.exports = function(){
-   return 'Hello world!';
+module.exports = function(sheet){
+   var result = [];
+   var row;
+   var rowNum;
+   var colNum;
+   for(rowNum = sheet['!range'].s.r; rowNum <= sheet['!range'].e.r; rowNum++){
+      row = [];
+      for(colNum=sheet['!range'].s.c; colNum<=sheet['!range'].e.c; colNum++){
+         var nextCell = sheet[
+            xlsx.utils.encode_cell({r: rowNum, c: colNum})
+         ];
+         if( typeof nextCell === 'undefined' ){
+            row.push(void 0);
+         } else row.push(nextCell.w);
+      }
+      result.push(row);
+   }
+   return result;
 };
